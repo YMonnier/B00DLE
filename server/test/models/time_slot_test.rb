62a6d73be15e59ed4c2942b7cf2ccd9e513c0676
Ysee Monnier <yseemonnier@gmail.com>
2016-10-08 22:41:01 +0200

    add time slot test model

--- server/test/models/time_slot_test.rb
+++ server/test/models/time_slot_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TimeSlotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test 'should save time slot model' do
+        timeSlot = TimeSlot.new
+        timeSlot.from = '25-04-2016 08:15'
+        timeSlot.to = '25-04-2016 09:15'
+        timeSlot.opinion_poll_id = 123
+        assert timeSlot.valid?
+    end
 end
