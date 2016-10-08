f89a0da7a61b5bec247c8d00b69814e0397575bf
Ysee Monnier <yseemonnier@gmail.com>
2016-10-08 20:53:04 +0200

    add application test model

--- server/test/models/application_test.rb
+++ server/test/models/application_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ApplicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test 'should save application model' do
+        app = Application.new
+        app.id = 2355642145453
+        assert app.valid?
+    end
+
+    test 'should not save application model' do
+        app = Application.new
+        assert_not app.valid?
+        app.id = 'ZEFGTR'
+        assert_not app.valid?
+    end
 end
