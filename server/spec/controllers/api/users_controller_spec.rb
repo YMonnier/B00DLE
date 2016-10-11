e878bfdbf112148d5e619427083c757693830527
Ysee Monnier <yseemonnier@gmail.com>
2016-10-11 17:56:09 +0200

    change users test, improve tests for resgistration

--- server/spec/controllers/api/users_controller_spec.rb
+++ server/spec/controllers/api/users_controller_spec.rb
@@ -23,6 +23,57 @@ RSpec.describe Api::UsersController, :type => :controller do
       it 'should be the same name' do
         expect(@user_response[:name]).to eql @user_attr[:name]
       end
+
+      it { should respond_with 201 }
+    end
+
+    context 'when is failed without the same password confirmation' do
+      before(:each) do
+        @user_attr = FactoryGirl.attributes_for :user
+        post :create, params: @user_attr
+        @json = json_response
+        @user_response = @json[:data]
+      end
+
+      it 'should not be same password' do
+        expect(@json[:succeed]).to eql false
+      end
+
+      it { should respond_with 400 }
+    end
+
+    context 'when is failed without the email' do
+      before(:each) do
+        @user_attr = FactoryGirl.attributes_for :user
+        @user_attr[:password_confirmation] = @user_attr[:password]
+        @user_attr[:email] = ''
+        post :create, params: @user_attr
+        @json = json_response
+        @user_response = @json[:data]
+      end
+
+      it 'should not be same password' do
+        expect(@json[:succeed]).to eql false
+      end
+
+      it { should respond_with 400 }
+    end
+
+    context 'when is failed without the name' do
+      before(:each) do
+        @user_attr = FactoryGirl.attributes_for :user
+        @user_attr[:password_confirmation] = @user_attr[:password]
+        @user_attr[:name] = ''
+        post :create, params: @user_attr
+        @json = json_response
+        @user_response = @json[:data]
+      end
+
+      it 'should not be same password' do
+        expect(@json[:succeed]).to eql false
+      end
+
+      it { should respond_with 400 }
     end
   end
 end
