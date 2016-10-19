07c019fd2f2cfe3e9956c5a14b3b8ff27d4a26cd
Ysee Monnier <yseemonnier@gmail.com>
2016-10-19 20:10:24 +0200

    change validations model - remove uniqueness email

--- server/app/models/invitation.rb
+++ server/app/models/invitation.rb
@@ -3,12 +3,6 @@ class Invitation < ApplicationRecord
 
     validates :email,
             presence: true,
-            uniqueness: true,
             format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
-            allow_blank: false
-
-    validates :opinion_poll_id,
-            presence: true,
-            numericality: true,
             allow_blank: false
 end
