b1aef316bcdbaae64c0e7f7b5ad259a36341fc5f
Ysee Monnier <yseemonnier@gmail.com>
2016-10-19 20:11:28 +0200

    change database schema

--- server/db/migrate/301_create_time_slots.rb
+++ server/db/migrate/301_create_time_slots.rb
@@ -1,8 +1,8 @@
 class CreateTimeSlots < ActiveRecord::Migration[5.0]
   def change
     create_table :time_slots do |t|
-      t.date :from
-      t.date :to
+      t.datetime :from
+      t.datetime :to
       t.belongs_to :opinion_poll, index: true, foreign_key: true
       t.timestamps
     end
--- server/db/migrate/500_create_invitations.rb
+++ server/db/migrate/500_create_invitations.rb
@@ -1,6 +1,6 @@
 class CreateInvitations < ActiveRecord::Migration[5.0]
   def change
-    create_table :invitations, id: false  do |t|
+    create_table :invitations do |t|
       t.belongs_to :opinion_poll, index: true, foreign_key: true
       t.string :email
       t.timestamps
