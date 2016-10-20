ed98a5d0d0e80908e550cd82845e8004db28a506
Ysee Monnier <yseemonnier@gmail.com>
2016-10-20 19:22:51 +0200

    add documentation

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::API
   #include ActionController::Serialization
   include Knock::Authenticable
 
+  ##
+  #
+  # Return json request
+  # with status 400/Bad Request.
+  # Parameters object: Error message.
+  #
+  ##
   def bad_request object
     return render json: {
         succeed: false,
@@ -9,6 +16,13 @@ class ApplicationController < ActionController::API
     }, status: 400
   end
 
+  ##
+  #
+  # Return json request
+  # with status 201/Created.
+  # Parameters object: Object created.
+  #
+  ##
   def created_request object
     return render json: {
         succeed: true,
@@ -16,6 +30,13 @@ class ApplicationController < ActionController::API
     }, status: 201
   end
 
+  ##
+  #
+  # Return json request
+  # with status 200/OK.
+  # Parameters object: Object wanted.
+  #
+  ##
   def ok_request object
     return render json: {
         succeed: true,
