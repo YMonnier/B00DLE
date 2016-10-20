a8184d7cea1817241df02450c3c92f732ffdaad4
Ysee Monnier <yseemonnier@gmail.com>
2016-10-20 23:22:31 +0200

    creating two users :: seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+2.times do |i|
+  User.create(id: i, name: FFaker.name, email: "admin#{i}@b00dle.com", password: 'secret1234')
+end
