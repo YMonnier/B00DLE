93029cf1018eba5768fad535fb91c5e8311e2cd8
Ysee Monnier <yseemonnier@gmail.com>
2016-10-25 14:31:39 +0200

    improve opinion poll model tests

--- server/spec/models/opinion_poll_spec.rb
+++ server/spec/models/opinion_poll_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
-
+require 'pp'
 RSpec.describe OpinionPoll, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @opinion_poll = FactoryGirl.build :opinion_poll }
+  subject { @opinion_poll }
+
+
+  it { should respond_to :title }
+  it { should respond_to :description }
+  it { should respond_to :place }
+  it { should respond_to :close }
+  it { should respond_to :user_id }
+
+  it { should be_valid }
+
+  it { should_not allow_value('').for :title }
+  it { should_not allow_value('').for :description }
+  it { should_not allow_value('').for :place}
+  it { should_not allow_value(nil).for :user_id}
+  it { should allow_value(true).for :close }
 end
