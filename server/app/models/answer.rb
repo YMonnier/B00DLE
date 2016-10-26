b8cc35b333292b1fc47fe6b09f3f036faf8c9ec7
Ysee Monnier <yseemonnier@gmail.com>
2016-10-26 12:37:36 +0200

    add test answer POST #create

--- server/app/models/answer.rb
+++ server/app/models/answer.rb
@@ -1,11 +1,12 @@
 class Answer < ApplicationRecord
-    belongs_to :opinion_poll
-    has_many :time_slots, :through => :answer_time_slots
+  belongs_to :opinion_poll
+  has_many :answer_time_slots
+  has_many :time_slots, :through => :answer_time_slots
 
-    validates :name, presence: true, allow_blank: false
+  validates :name, presence: true, allow_blank: false
 
-    validates :opinion_poll_id,
-                presence: true,
-                numericality: true,
-                allow_blank: false
+  validates :opinion_poll_id,
+            presence: true,
+            numericality: true,
+            allow_blank: false
 end
--- server/spec/controllers/api/answers_controller_spec.rb
+++ server/spec/controllers/api/answers_controller_spec.rb
@@ -4,39 +4,43 @@ RSpec.describe Api::AnswersController, :type => :controller do
   describe 'POST #create' do
     context 'when is successfully created' do
       before do
-        @user = FactoryGirl.create :user
+        @opinion_poll = FactoryGirl.create :opinion_poll #, user: @user
 
-        token = generate_token @user
-        api_authorization_header(token)
+        @times = []
+        4.times {
+          t = FactoryGirl.create :time_slot, opinion_poll: @opinion_poll
+          @times.append t.id
+        }
 
-        @opinion_poll = FactoryGirl.create :opinion_poll, user: @user
-
-        @time = FactoryGirl.create :time_slot, opinion_poll: @opinion_poll
-
-        pp '-- > @opinion_poll'
-        pp @opinion_poll
-        pp '-- > @time'
-        pp @time
 
         parameters = {
             app_id: '5E:FF:56:A2:AF:15',
             name: 'John',
             opinion_poll_id: @opinion_poll.id,
-            times: [@time.id]
+            times: @times
         }
-        pp parameters
 
         post :create, params: parameters
         @json = json_response
-        pp @json
         @data = json_response[:data]
         @user_response = @json[:data]
       end
 
 
-      #it 'succeed should be succeed' do
-      #  expect(@json[:succeed]).to eql true
-      #end
+      it 'succeed should be succeed' do
+        expect(@json[:succeed]).to eql true
+      end
+
+      it 'opinion poll id should be the same' do
+        expect(@data[:opinion_poll][:id]).to eql @opinion_poll.id
+      end
+
+      it 'time slots id should be the same' do
+        expect(@data[:time_slots].size).to eql 4
+        @times.each_with_index do |id, index|
+          expect(@data[:time_slots][index][:id]).to eql id
+        end
+      end
 
       it { should respond_with 201 }
     end
